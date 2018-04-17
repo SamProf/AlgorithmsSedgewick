40867e3268719ec5063f687284e36cc8bef571f1
Vladimir Samoilenko <wsamprof@gmail.com>
2018-04-17 16:10:25 +0200

    UnionFindSolution1

---
+++ AlgorithmsSedgewick/UnionFind/UnionFindSolution1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace AlgorithmsSedgewick.UnionFind
+{
+    public class UnionFindSolution1
+    {
+        private readonly int n;
+        private readonly int[] a;
+        private readonly int[] w;
+
+        public class LogItem
+        {
+            public int Time;
+            public int Value1;
+            public int Value2;
+        }
+
+        public bool Finded;
+        public int Value;
+
+
+
+        private int GetRoot(int v)
+        {
+            while (v!=a[v])
+            {
+                a[v] = a[a[v]];
+                v = a[v];
+            }
+            return v;
+        }
+
+        public void Connect(LogItem log)
+        {
+            var r1 = GetRoot(log.Value1);
+            var r2 = GetRoot(log.Value2);
+            var wc = 0;
+            if (r1 != r2)
+            {
+                if (w[r1] <= w[r2])
+                {
+                    a[r1] = r2;
+                    w[r2] += w[r1];
+                    wc = w[r2];
+                }
+                else
+                {
+                    a[r2] = r1;
+                    w[r1] += w[r2];
+                    wc = w[r1];
+                }
+            }
+
+            if (wc == n)
+            {
+                Finded = true;
+                Value = log.Time;
+            }
+        }
+
+        public UnionFindSolution1(int n)
+        {
+            this.n = n;
+            a = new int[n];
+            w = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = i;
+                w[i] = 1;
+            }
+        }
+    }
+}
---
+++ AlgorithmsSedgewick/UnionFind/UnionFindTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AlgorithmsSedgewick.UnionFind
+{
+    [TestFixture]
+    public class UnionFindTests
+    {
+        [Test]
+        public void Test1()
+        {
+            var n = 1000;
+            var rnd =new Random();
+            var time = 0;
+            var s = new UnionFindSolution1(n);
+
+            while (!s.Finded)
+            {
+                s.Connect(new UnionFindSolution1.LogItem()
+                {
+                    Time = time++,
+                    Value1 = rnd.Next(0, n),
+                    Value2 = rnd.Next(0, n)
+                });
+            }
+            Console.Write(s.Value);
+
+        }
+    }
+
+
+    
+}
